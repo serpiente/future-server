f9b681a08defdafbc2838e433e1db93c64633fe7
Sergio Villegas <serpiente1991@gmail.com>
2023-12-04 22:10:25 +0100

    second commit, super basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io::Read;
+use std::io::{self, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:3000").unwrap();
+
+    loop {
+        let (connection, _) = listener.accept().unwrap();
+
+        if let Err(e) = handle_connection(connection) {
+            println!("failed to handle connection: {e}")
+        }
+    }
+}
+
+
+fn handle_connection(mut connection: TcpStream) -> io::Result<()> {
+    let mut read = 0;
+    let mut request = [0u8; 1024];
+
+    loop {
+        // try reading from the stream
+        let num_bytes = connection.read(&mut request[read..])?;
+
+        // keep track of how many bytes we've read
+        read += num_bytes;
+
+        // have we reached the end of the request?
+        if request.get(read - 4..read) == Some(b"\r\n\r\n") {
+            break;
+        }
+    }
+
+    Ok(())
 }
